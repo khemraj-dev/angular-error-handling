93cfd7aa6e9729969384adbf47722d90cd4dc900
khemraj-dev <khemraj.47billion@gmail.com>
2019-05-26 12:51:05 +0530

    add angular error handling with interceptor

---
+++ src/app/app-service.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, retry, catchError } from 'rxjs/operators'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppServiceService {
+  base_Url = 'https://api.chucknorris.io/jokes';
+
+  constructor(private http: HttpClient) { }
+
+  getJokes(): Observable<any> {
+    return this.http.get(`${this.base_Url}/random`); // .pipe(retry(3), catchError(this.handleError));
+  }
+
+  extractJokes() {
+    this.getJokes().subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  // handleError(err) {
+  //   let errorMessage = '';
+  //   if (err.error instanceof ErrorEvent) {
+  //     // client-side error
+  //     errorMessage = `Error: ${err.message}`;
+  //   } else {
+  //     // server-side error
+  //     errorMessage = `Error Code: ${err.status}\nMessage: ${err.message}`;
+  //   }
+  //   alert(errorMessage);
+  //   return throwError(errorMessage);
+  // }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpInterceptorClass } from './httpInterceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpInterceptorClass,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
